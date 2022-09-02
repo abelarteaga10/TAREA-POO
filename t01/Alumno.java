aa2029b63e5a42975e97fd28ffded33a92decff6
abelarteaga10 <77707727+abelarteaga10@users.noreply.github.com>
2022-09-02 12:44:24 -0500

    Update Alumno.java

--- t01/Alumno.java
+++ t01/Alumno.java
@@ -3,7 +3,7 @@ public class Alumno{
 
     int codigo=2020210330;
     String  nombre="Abel Arteaga";
-    int edad=23;
+    int edad=19;
 
     public String mostrarnombre(){
         return nombre;
